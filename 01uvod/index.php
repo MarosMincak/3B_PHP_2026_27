14aaa47b6b7fdd5aaf033c508904238bd4a309e2
MarosMincak <maros.mincak11@gmail.com>
2026-09-10 11:08:31 +0200

    Naučili sme sa pouzivat print

--- 01uvod/index.php
+++ 01uvod/index.php
@@ -1,3 +1,30 @@
-<?php 
-echo "ahoj";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+  echo "toto je mimo uvodzoviek";
+  ?>
+
+  <div>
+    <p>ahoj škola</p>
+  </div>
+
+  <?php 
+  echo "toto je dalsie echo";
+  ?>
+
+
+  <?php 
+  echo "<p> tento kod je vnutro php </p> ";
+
+  print("aj toto je moj vypis pomocou funkcie print");
+  ?>
+
+  
+</body>
+</html>
